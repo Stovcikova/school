f10dc375ce74b6c0af2b91b8291292ecf685aa66
Stovcikova <henrieta.stovcikova@kosickaakademia.sk>
2020-11-11 10:53:08 +0100

    file

---
+++ MainFile.java
@@ -0,0 +1,9 @@
+package sk.kosickaakademia.stovcikova.files;
+
+public class Main {
+    public static void main(String[] args) {
+        RWFile rwFile = new RWFile();
+        rwFile.copyFileWithCapital(" resours/a.txt");
+
+    }
+}
